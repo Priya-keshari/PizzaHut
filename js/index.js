25f42488b03e96fbeb3a744f75bce3fe6910d43f
Priya keshari <112851089+Priya-keshari@users.noreply.github.com>
2024-10-02 22:50:27 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
+emailjs.init("99F_N_yJTLAmE21wu")
 
+document.querySelector("#form").addEventListener("submit", (event)=>{
+    event.preventDefault()
+
+    emailjs.send("service_bqtqjk7","template_nsdxgfw",{
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        dishes: document.getElementById("dish").value,
+        quantity: document.getElementById("quantity").value,
+    })
+    .then(()=>{
+        console.log("message sents")
+        alert("your message sent succeefully");
+    })
+    .catch(()=>{
+        console.log("error msg not sent")
+    })
+})
